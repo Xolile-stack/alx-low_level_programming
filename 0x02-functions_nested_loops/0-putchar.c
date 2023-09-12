f3082e403d52ac71e2b6fbb626599147cd12708b
Xolile-stack <xolilengcobo246@gmail.com>
2023-09-12 11:19:31 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include "main.h"
 /**
  * main-Entry point 
  *
@@ -6,13 +6,14 @@
  */
 int main (void)
 {
-	char text [] = "_putchar\n";
-	int i = 0;
-	while (text[i] != '\0')
-	{
-		write (1, &text[i], 1);
-		i++
-	}
-
-	return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('/n);
+	return(0);
 }

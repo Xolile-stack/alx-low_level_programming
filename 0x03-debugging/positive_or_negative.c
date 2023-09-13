6ab6eac2ee3eddfd8a6a0355725bbdf6a09691c7
Xolile-stack <xolilengcobo246@gmail.com>
2023-09-13 08:41:26 -0700

    1st tasks

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "main.h"
+
+// Function declaration
 void positive_or_negative(int i);
-{
+
+// Function definition
+void positive_or_negative(int i) {
 	if (i > 0)
-		printf("%d is positive\n", i);
-	else if (i==0)
-		printf("%d is zero\n, i");
-	else
-		printf("%d is negative\n, i");
-
+	    printf("%d is positive\n", i);
+    else if (i == 0)
+	    printf("%d is zero\n", i); // Fixed the printf format string
+    else
+	    printf("%d is negative\n", i); // Fixed the printf format string
 }
-
-
